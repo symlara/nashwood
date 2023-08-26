b38d7ce6b81517acc85bc644fa30fb8789a8de6c
aaliyahsymlar <asym14@gmail.com>
2023-08-26 11:27:38 -0500

    added wordmark to replace logo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import nashwood from "./assets/nashwood-nw-icon-logo-black-rgb-640px-w-72ppi.png";
 import nashwood2 from "./assets/nashwood-nw-icon-logo-white-rgb-640px-w-72ppi.png";
+import nashwoodwordmark from "./assets/nashwood-nw-wordmark-logo-white-rgb-640px-w-72ppi.png";
+
 import "./App.css";
 
 function App() {
@@ -9,7 +11,16 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center">
-        <img src={nashwood2} alt="nashwood-logo" width={150} height={100} />
+        <img src={nashwoodwordmark} alt="nashwood-workmark" />
+      </div>
+      <div className="flex justify-center items-center">
+        {/* <img
+          src={nashwood2}
+          alt="nashwood-logo"
+          width={100}
+          height={100}
+          className="md:mr-[1700px]"
+        /> */}
       </div>
     </>
   );
